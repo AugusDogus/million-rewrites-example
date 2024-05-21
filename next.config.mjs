91fde430af1f0754f36729516585fd2a7fc309f9
AugusDogus <9794679+AugusDogus@users.noreply.github.com>
2024-05-21 11:38:58 -0500

    feat: break million lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
-import MillionLint from '@million/lint';
+import MillionLint from "@million/lint";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: "/:path*",
+          destination: `https://example.com/:path*`,
+        },
+      ],
+    };
+  },
 };
 export default MillionLint.next()(nextConfig);
